15bc49ac862af52b18ef2a07f6ef3a3d12167b58
carloscamacho <carlosalvarez9805@gmail.com>
2023-01-18 16:27:10 -0600

    Project base

---
+++ database/migrations/2023_01_17_225611_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->double('price');
+            $table->unsignedBigInteger('type_id');
+            $table->unsignedBigInteger('flavor_id');
+            
+            $table->foreign('type_id')
+                  ->references('id')
+                  ->on('types');
+
+            $table->foreign('flavor_id')
+                  ->references('id')
+                  ->on('flavors');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
