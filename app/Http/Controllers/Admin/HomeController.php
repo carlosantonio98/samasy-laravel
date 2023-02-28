4b0abb6a3af2bfee7204ab1c98d96d9c26e545d1
carloscamacho <carlosalvarez9805@gmail.com>
2023-02-28 17:14:47 -0600

    Add information to the seller dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -7,7 +7,6 @@ use App\Models\Flavor;
 use App\Models\Product;
 use App\Models\Sale;
 use App\Models\Type;
-use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -20,98 +19,130 @@ class HomeController extends Controller
     public function index()
     {
 
-        $user = User::find(Auth()->user()->id);
+        $user = auth()->user();
 
-        if ($user->hasRole('Admin')) {
-            $amountSales    = Sale::count();
-            $amountProducts = Product::count();
-            $amountTypes    = Type::count();
-            $amountFlavors  = Flavor::count();
-            
-            $amountSalesByDay    = Sale::whereYear('sales.created_at', date('Y')) 
-                                    ->whereMonth('sales.created_at', date('m'))
-                                    ->whereDay('sales.created_at', date('d'))
-                                    ->count();
+        $amountSales    = new Sale();
+        $amountProducts = Product::count();
+        $amountTypes    = Type::count();
+        $amountFlavors  = Flavor::count();
 
-            $totalMoney     = Sale::join('products', 'sales.product_id', '=', 'products.id')
-                                    ->selectRaw('SUM(products.price) as total_money')
-                                    ->get()[0]
-                                    ->total_money;
-
-                                    
-
-            $monthMoney     = Sale::join('products', 'sales.product_id', '=', 'products.id')
-                                    ->whereYear('sales.created_at', date('Y'))
-                                    ->whereMonth('sales.created_at', date('m'))
-                                    ->selectRaw('SUM(products.price) as month_money, MONTH(sales.created_at) as date_month')
-                                    ->groupby('date_month')
-                                    ->get();
+        $amountSalesByDay = Sale::whereYear('sales.created_at', date('Y')) 
+            ->whereMonth('sales.created_at', date('m'))
+            ->whereDay('sales.created_at', date('d'));
 
 
-            $dayMoney       = Sale::join('products', 'sales.product_id', '=', 'products.id')
-                                    ->whereYear('sales.created_at', date('Y')) 
-                                    ->whereMonth('sales.created_at', date('m'))
-                                    ->whereDay('sales.created_at', date('d'))
-                                    ->selectRaw('SUM(products.price) as day_money, DAY(sales.created_at) as date_day')
-                                    ->groupby('date_day')
-                                    ->get();
+        $totalMoney = Sale::join('products', 'sales.product_id', '=', 'products.id')
+            ->selectRaw('SUM(products.price) as total_money');
+
+        
+        $monthMoney = Sale::join('products', 'sales.product_id', '=', 'products.id')
+            ->selectRaw('SUM(products.price) as month_money, MONTH(sales.created_at) as date_month')
+            ->whereYear('sales.created_at', date('Y'))
+            ->whereMonth('sales.created_at', date('m'));
 
 
-            $monthMoney = $monthMoney->count() > 0 ? $monthMoney[0]->month_money : 0;
-            $dayMoney   = $dayMoney->count() > 0 ? $dayMoney[0]->day_money : 0;
-            
+        $dayMoney = Sale::join('products', 'sales.product_id', '=', 'products.id')
+            ->selectRaw('SUM(products.price) as day_money, DAY(sales.created_at) as date_day')
+            ->whereYear('sales.created_at', date('Y')) 
+            ->whereMonth('sales.created_at', date('m'))
+            ->whereDay('sales.created_at', date('d'));
 
 
-            // Chart one - Sales current year by months
-            $totalMoneyByMonths   = Sale::join('products', 'sales.product_id', '=', 'products.id')
-                                    ->whereYear('sales.created_at', date('Y'))
-                                    ->selectRaw('SUM(products.price) as month_money, MONTH(sales.created_at) as date_month')
-                                    ->groupby('date_month')
-                                    ->get();
-
-            $dataChartOne = array_fill(0, 12, 0);
-
-            foreach($totalMoneyByMonths as $item) {
-                $dataChartOne[$item['date_month'] - 1] = $item['month_money'];
-            }
-
-            $chartOne = json_encode(
-                [
-                    'series' => [
-                        'name' => 'Sales',
-                        'data' => $dataChartOne,
-                    ],
-                    'categories' => ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-                ]
-            );
+        $sales = Sale::with('product')->latest()->limit(5);
 
 
-            // Chart two - Number of sales products
-            $numberProductsByType = Sale::join('products', 'sales.product_id', '=', 'products.id')
-                                        ->selectRaw('products.name,  COUNT(*) as salesTotal')
-                                        ->groupby('products.name')
-                                        ->get();
-
-            $seriesChartTwo = [];
-            $labelsChartTwo = [];
-
-            foreach ($numberProductsByType as $product) {
-                array_push($seriesChartTwo, $product['salesTotal']);
-                array_push($labelsChartTwo, $product['name']);
-            }
-
-            $chartTwo = json_encode(
-                [
-                    'series' => $seriesChartTwo,
-                    'labels' => $labelsChartTwo
-                ]
-            );
-
-            $sales = Sale::with('product')->latest()->limit(5)->get();
-
-            return view('admin.index', compact('amountSales', 'amountProducts', 'amountTypes', 'amountFlavors', 'amountSalesByDay', 'totalMoney', 'monthMoney', 'dayMoney', 'chartOne', 'chartTwo', 'sales', 'user'));
+        if (!$user->hasRole('Admin')) {
+            $amountSales      = $amountSales->whereUserId($user->id);
+            $amountSalesByDay = $amountSalesByDay->whereUserId($user->id);
+            $totalMoney       = $totalMoney->where('sales.user_id', $user->id);
+            $monthMoney       = $monthMoney->where('sales.user_id', $user->id);
+            $dayMoney         = $dayMoney->where('sales.user_id', $user->id);
+            $sales            = $sales->whereUserId($user->id);
         }
 
-        return view('admin.index', compact('user'));
+
+        $amountSales      = $amountSales->count();
+        $amountSalesByDay = $amountSalesByDay->count();
+        $totalMoney       = $totalMoney->get()[0]->total_money;
+        $monthMoney       = $monthMoney->groupby('date_month')->get();
+        $dayMoney         = $dayMoney->groupby('date_day')->get();
+        $sales            = $sales->get();
+
+        $monthMoney = $monthMoney->count() > 0 ? $monthMoney[0]->month_money : 0;
+        $dayMoney   = $dayMoney->count() > 0 ? $dayMoney[0]->day_money : 0;
+
+        $chartOne = $this->getDataChartOne();
+        $chartTwo = $this->getDataChartTwo();
+
+        return view(
+            'admin.index', compact('amountSales', 'amountProducts', 'amountTypes', 'amountFlavors', 'amountSalesByDay', 'totalMoney', 'monthMoney', 'dayMoney', 'chartOne', 'chartTwo', 'sales', 'user'));
+    }
+    
+    public function getDataChartOne()
+    {
+        $user = auth()->user();
+
+        // Sales current year by months
+        $totalMoneyByMonths = Sale::join('products', 'sales.product_id', '=', 'products.id')
+            ->selectRaw('SUM(products.price) as month_money, MONTH(sales.created_at) as date_month')
+            ->whereYear('sales.created_at', date('Y'));
+
+        if (!$user->hasRole('Admin')) {
+            $totalMoneyByMonths = $totalMoneyByMonths->where('sales.user_id', $user->id);
+        }
+
+        $totalMoneyByMonths = $totalMoneyByMonths->groupby('date_month')->get();
+
+        // Data
+        $dataChartOne = array_fill(0, 12, 0);
+    
+        foreach($totalMoneyByMonths as $item) {
+            $dataChartOne[$item['date_month'] - 1] = $item['month_money'];
+        }
+    
+        $chartOne = json_encode(
+            [
+                'series' => [
+                'name' => 'Sales',
+                'data' => $dataChartOne,
+            ],
+                'categories' => ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            ]
+        );
+
+        return $chartOne;
+    }
+
+    public function getDataChartTwo()
+    {
+        $user = auth()->user();
+
+        // Number of sales products
+        $numberProductsByType = Sale::join('products', 'sales.product_id', '=', 'products.id')
+            ->selectRaw('products.name,  COUNT(*) as salesTotal');
+
+        if (!$user->hasRole('Admin')) {
+            $numberProductsByType = $numberProductsByType->where('sales.user_id', $user->id);
+        }
+
+        $numberProductsByType = $numberProductsByType->groupby('products.name')->get();
+
+        // Data
+        $seriesChartTwo = [];
+        $labelsChartTwo = [];
+
+        foreach ($numberProductsByType as $product) {
+            array_push($seriesChartTwo, $product['salesTotal']);
+            array_push($labelsChartTwo, $product['name']);
+        }
+
+        $chartTwo = json_encode(
+            [
+                'series' => $seriesChartTwo,
+                'labels' => $labelsChartTwo
+            ]
+        );
+
+        return $chartTwo;
     }
 }
