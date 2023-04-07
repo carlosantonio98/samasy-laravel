8ed070921012d657a039b45a1a599e843c5bf910
carloscamacho <carlosalvarez9805@gmail.com>
2023-04-06 18:09:33 -0600

    Add effect to sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,13 +14,18 @@ const toggleSidebarMobile = document.querySelector('#toggleSidebarMobile');
 if ( toggleSidebarMobile ) {
     toggleSidebarMobile.addEventListener('click', () => {
         sidebarBackdrop.classList.toggle('hidden');
-        sidebar.classList.toggle('hidden');
+
+        if ( sidebar.classList.contains('left-[-100%]') ) {
+            sidebar.classList.replace('left-[-100%]', 'left-0');
+        } else {
+            sidebar.classList.replace('left-0', 'left-[-100%]');
+        }
     });
 }
 
 if ( sidebarBackdrop ) {
     sidebarBackdrop.addEventListener('click', (event) => {
+        sidebar.classList.replace('left-0', 'left-[-100%]');
         event.target.classList.add('hidden');
-        sidebar.classList.add('hidden');
     });
 }
